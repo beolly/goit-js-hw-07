b8c787230de18ad2a56891521157c317cb995927
beeolly <belenkod55@gmail.com>
2025-12-06 17:50:20 +0200

    improve validation logic

--- js/task-4.js
+++ js/task-4.js
@@ -1,20 +1,18 @@
 const form = document.querySelector(".login-form");
-form.addEventListener("submit", function (event) {
+form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const email = form.elements.email.value;
   const password = form.elements.password.value.toString();
 
-  if (email === "" || password === "") {
+  if (email && password) {
+    const formData = {
+      email: email,
+      password: password,
+    };
+    console.log(formData);
+    form.reset();
+  } else {
     alert("All form fields must be filled in");
   }
-
-  const formData = {
-    email: email,
-    password: password,
-  };
-
-  console.log(formData);
-
-  form.reset();
 });
