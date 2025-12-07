d84a4f1eb034739ca9fd93fb9c77ea01d43438b4
beeolly <belenkod55@gmail.com>
2025-12-07 11:14:36 +0200

    trim whitespace from email and password fields on form submission

--- js/task-4.js
+++ js/task-4.js
@@ -2,8 +2,8 @@ const form = document.querySelector(".login-form");
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const email = form.elements.email.value;
-  const password = form.elements.password.value.toString();
+  const email = form.elements.email.value.trim();
+  const password = form.elements.password.value.toString().trim();
 
   if (email && password) {
     const formData = {
